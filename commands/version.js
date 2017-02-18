c6f4676bb04f1997494c59dabe7ff7e66c66164b
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2017-02-17 20:05:55 -0800

    show channel on version

--- commands/version.js
+++ commands/version.js
@@ -4,7 +4,8 @@ const config = require('../lib/config')
 class Version extends Command {
   async run () {
     if (process.env.HEROKU_DEV) this.log(`HEROKU_DEV=${process.env.HEROKU_DEV}`)
-    this.log(`${config.name}/${config.version} (${process.platform}-${process.arch}) node-${process.version}`)
+    let channel = config.channel === 'stable' ? '' : ` ${config.channel}`
+    this.log(`${config.name}/${config.version}${channel} (${process.platform}-${process.arch}) node-${process.version}`)
   }
 }
 
