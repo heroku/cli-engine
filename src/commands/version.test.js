d9778531001ae8615bde9fc0bb4f1af87bf2ea99
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2017-02-22 16:36:37 -0800

    circling

--- src/commands/version.test.js
+++ src/commands/version.test.js
@@ -8,7 +8,6 @@ import Command from './version'
 
 test('shows the version', async function () {
   let cmd = new Command({argv: ['cli-engine', 'version'], mock: true})
-  await cmd.init()
-  await cmd.run()
+  await cmd._run()
   expect(cmd.stdout.output).toMatch(/^cli-engine/)
 })
