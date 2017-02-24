6e99ad4269cc495ab575d496959848670361b70b
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2017-02-23 16:42:00 -0800

    working on example

--- example/bin/run.js
+++ example/bin/run.js
@@ -1,3 +1,8 @@
-require('cli-engine')({
-  disableUpdate: `add update disable message here`
+const path = require('path')
+const CLI = require('cli-engine').default
+const cli = new CLI({
+  root: path.join(__dirname, '..'),
+  updateDisabled: `add update disable message here`,
+  argv: process.argv.slice(1)
 })
+cli.run()
