cf75ea9d3f38075d757e3cbd1ce57fce4567d01c
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2017-02-27 13:43:30 -0800

    more test coverage

---
+++ src/commands/help.test.js
@@ -0,0 +1,19 @@
+// @flow
+/* globals
+   test
+   expect
+ */
+
+import Command from './help'
+
+test('shows the topics', async function () {
+  let cmd = new Command({mock: true})
+  await cmd._run()
+  expect(cmd.stdout.output).toMatch(/^ plugins +# manage plugins$/m)
+})
+
+test('shows help about plugins', async function () {
+  let cmd = new Command({argv: ['cli-engine', 'help', 'plugins'], mock: true})
+  await cmd._run()
+  expect(cmd.stdout.output).toMatch(/^ +plugins:install PLUGIN +# installs a plugin into the CLI$/m)
+})
