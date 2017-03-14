5c58be1538826502c14010b217d66d4ea9adf11d
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2017-03-13 18:31:58 -0700

    exit with 127

--- src/not_found.js
+++ src/not_found.js
@@ -4,8 +4,8 @@ import {Base} from 'cli-engine-command'
 
 export default class NotFound extends Base {
   async run () {
-    throw new Error(`${this.color.yellow(this.config.argv[1])} is not a heroku command.
+    this.error(`${this.color.yellow(this.config.argv[1])} is not a heroku command.
 Perhaps you meant ????
-Run ${this.color.cmd('heroku help')} for a list of available commands.`)
+Run ${this.color.cmd('heroku help')} for a list of available commands.`, 127)
   }
 }
