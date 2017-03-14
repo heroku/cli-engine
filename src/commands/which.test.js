4b4e04f29fa166450018937f41aa7b1b4135c2d2
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2017-03-13 22:18:00 -0700

    run in band

--- src/commands/which.test.js
+++ src/commands/which.test.js
@@ -1,10 +1,22 @@
 // @flow
 
 import Which from './which'
+import Plugins from '../plugins'
+import {Config} from 'cli-engine-command'
+
+function plugins () {
+  return new Plugins(new Config({mock: true}))
+}
+
+async function install (plugin) {
+  await plugins().install(plugin)
+}
 
 test('finds a plugin command', async () => {
-  let cmd = await Which.run(['cli:test'], {mock: true})
-  expect(cmd.stdout.output).toContain('cli-engine-example-plugin')
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000
+  await install('heroku-debug')
+  let cmd = await Which.run(['debug'], {mock: true})
+  expect(cmd.stdout.output).toContain('heroku-debug')
 })
 
 test('finds a builtin command', async () => {
