f1716d341bdb6488a99c5309e066c1a3ad177685
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2017-03-17 20:22:20 -0700

    fixed things

--- src/legacy.js
+++ src/legacy.js
@@ -1,6 +1,6 @@
 // @flow
 
-import Command, {type Flag, type Arg} from 'cli-engine-command'
+import Command, {type Arg, BooleanFlag, StringFlag} from 'cli-engine-command'
 import {AppFlag, RemoteFlag} from 'cli-engine-command/lib/flags/app'
 import OrgFlag from 'cli-engine-command/lib/flags/org'
 import Heroku from 'cli-engine-command/lib/heroku'
@@ -12,7 +12,11 @@ export type LegacyContext = {
 export type LegacyFlag = {
   name: string,
   description?: string,
-  char?: string
+  char?: string,
+  hasValue?: boolean,
+  hidden?: boolean,
+  required?: boolean,
+  optional?: boolean
 }
 
 export type LegacyCommand = {
@@ -33,7 +37,7 @@ export type LegacyCommand = {
   run: (ctx: LegacyContext) => Promise<any>
 }
 
-export function convertFromV5 (c: LegacyCommand): Class<Command> {
+export function convertFromV5 (c: LegacyCommand): Class<Command<*>> {
   if (!c.topic) throw new Error('command has no topic')
   class V5 extends Command {
     static topic = c.topic
@@ -41,30 +45,50 @@ export function convertFromV5 (c: LegacyCommand): Class<Command> {
     static description = c.description
     static hidden = c.hidden
     static args = c.args || []
-    static flags = c.flags || []
+    static flags = (c.flags || []).reduce((flags, flag) => {
+      let opts = {
+        char: (flag.char: any),
+        description: flag.description,
+        hidden: flag.hidden,
+        required: flag.required,
+        optional: flag.optional
+      }
+      flags[flag.name] = flag.hasValue ? StringFlag(opts) : BooleanFlag(opts)
+      return flags
+    }, {})
     static variableArgs = !!c.variableArgs
     static help = c.help
 
     heroku = new Heroku(this, {required: false})
-    app = new App(this, {required: c.needsApp})
-    org = new Org(this, {required: c.needsOrg})
 
     run () {
+      let flags: any = this.flags
+      let args: (string[] | {[k: string]: string}) = this.argv
+      if (!c.variableArgs) {
+        // turn args into object v5 expects
+        args = {}
+        for (let i = 0; i < this.argv.length; i++) {
+          args[this.constructor.args[i].name] = this.argv[i]
+        }
+      }
       const ctx = {
         supportsColor: this.color.enabled,
         auth: {},
         debug: this.config.debug,
-        flags: this.flags,
-        args: c.variableArgs ? this.argv : this.args,
-        app: this.app.name,
-        org: this.org.name
+        flags,
+        args,
+        app: flags.app,
+        org: flags.org
       }
       ctx.auth.password = this.heroku.auth
       return c.run(ctx)
     }
   }
 
-  if (c.needsApp || c.wantsApp) V5.flags.push(AppFlag, RemoteFlag)
-  if (c.needsOrg || c.wantsOrg) V5.flags.push(OrgFlag)
+  if (c.needsApp || c.wantsApp) {
+    V5.flags.app = AppFlag({required: !!c.needsApp})
+    V5.flags.remote = RemoteFlag()
+  }
+  if (c.needsOrg || c.wantsOrg) V5.flags.org = OrgFlag({required: !!c.needsOrg})
   return V5
 }
