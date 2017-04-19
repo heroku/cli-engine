2a9803293086c3f79138712e937a0d352d3d7da8
Ransom Briggs <rbriggs+github@heroku.com>
2017-04-19 16:33:49 -0500

    Factor out {user,builtin,core} plugins from plugin (#68)
    * Factor out {user,builtin,core} plugins from plugin
    
    * Make list() a property & remove path.resolve
    
    * Fix core_plugins comment

--- src/plugins.js
+++ src/plugins.js
@@ -4,12 +4,12 @@ import {type Config} from 'cli-engine-config'
 import Command, {Topic} from 'cli-engine-command'
 import type Output from 'cli-engine-command/lib/output'
 import path from 'path'
-import Yarn from './yarn'
-import lock from 'rwlockfile'
 import LinkedPlugins from './linked_plugins'
+import UserPlugins from './user_plugins'
+import BuiltinPlugins from './builtin_plugins'
+import CorePlugins from './core_plugins'
 import uniqby from 'lodash.uniqby'
 import {convertFromV5, convertFlagsFromV5, type LegacyCommand} from './legacy'
-import fs from 'fs-extra'
 import Cache, {type CachedCommand, type CachedPlugin, type CachedTopic} from './plugins_cache'
 
 type PluginType = | "builtin" | "core" | "user" | "link"
@@ -23,14 +23,10 @@ type ParsedTopic = | {
 
 type ParsedCommand = | LegacyCommand | Class<Command<*>>
 
-type ParsedPlugin = {
+export type ParsedPlugin = {
   topic?: ParsedTopic,
   topics?: ParsedTopic[],
   commands?: ParsedCommand[]
-}
-
-type PJSON = {
-  dependencies?: { [name: string]: string }
 }
 
 type PluginOptions = {
@@ -185,50 +181,27 @@ export default class Plugins {
   constructor (output: Output) {
     this.out = output
     this.config = output.config
-    this.yarn = new Yarn(output)
     this.cache = new Cache(output)
-    this.linkedPlugins = new LinkedPlugins(this)
 
-    let commandsPath = path.resolve(path.join(__dirname, 'commands'))
-    this.plugins = [new Plugin('builtin', commandsPath, this)]
-    .concat(this.linkedPlugins.list())
-    .concat(this.userPlugins)
-    .concat(this.corePlugins)
+    this.builtinPlugins = new BuiltinPlugins(this)
+    this.linkedPlugins = new LinkedPlugins(this)
+    this.userPlugins = new UserPlugins(this)
+    this.corePlugins = new CorePlugins(this)
+
+    this.plugins = this.builtinPlugins.list
+    .concat(this.linkedPlugins.list)
+    .concat(this.userPlugins.list)
+    .concat(this.corePlugins.list)
     this.cache.save()
   }
 
+  builtinPlugins: BuiltinPlugins
   linkedPlugins: LinkedPlugins
+  userPlugins: UserPlugins
+  corePlugins: CorePlugins
   plugins: Plugin[]
   cache: Cache
-  yarn: Yarn
   out: Output
-
-  get corePlugins (): Plugin[] {
-    let cli = this.config.pjson['cli-engine']
-    if (!cli) return []
-    return (cli.plugins || []).map(name => {
-      return new Plugin('core', path.join(this.config.root, 'node_modules', name), this)
-    })
-  }
-
-  get userPlugins (): Plugin[] {
-    const pjson = this.userPluginsPJSON
-    return entries(pjson.dependencies || {}).map(([name, tag]) => {
-      return new Plugin('user', this.userPluginPath(name), this, {tag})
-    })
-  }
-
-  get userPluginsPJSON (): PJSON {
-    try {
-      return fs.readJSONSync(this.userPluginsPJSONPath)
-    } catch (err) {
-      return { dependencies: {} }
-    }
-  }
-
-  saveUserPluginsPJSON (pjson: PJSON) {
-    fs.writeJSONSync(path.join(this.userPluginsPJSONPath), pjson)
-  }
 
   get commands (): CachedCommand[] {
     let commands = []
@@ -279,48 +252,24 @@ export default class Plugins {
     }
   }
 
-  async setupUserPlugins () {
-    const pjson = path.join(this.userPluginsDir, 'package.json')
-    const yarnrc = path.join(this.userPluginsDir, '.yarnrc')
-    fs.mkdirpSync(this.userPluginsDir)
-    if (!fs.existsSync(pjson)) fs.writeFileSync(pjson, JSON.stringify({private: true}))
-    if (!fs.existsSync(yarnrc)) fs.writeFileSync(yarnrc, 'registry "https://cli-npm.heroku.com/"')
-    await this.yarn.exec()
-  }
-
   async install (name: string, tag: string = 'latest') {
-    let unlock = await lock.write(this.lockfile, {skipOwnPid: true})
-    await this.setupUserPlugins()
     if (this.plugins.find(p => p.name === name && p.options.tag === tag)) throw new Error(`Plugin ${name} is already installed`)
-    this.addPackageToPJSON(name, tag)
-    await this.yarn.exec()
+    await this.userPlugins.install(name, tag)
     this.clearCache(name)
-    try {
-      // flow$ignore
-      let plugin = (require(this.userPluginPath(name)): ParsedPlugin)
-      if (!plugin.commands) throw new Error(`${name} does not appear to be a Heroku CLI plugin`)
-    } catch (err) {
-      await unlock()
-      this.out.error(err, false)
-      this.removePackageFromPJSON(name)
-      this.out.exit(1)
-    }
-    await unlock()
   }
 
   async update () {
-    await this.yarn.exec(['upgrade'])
-    this.clearCache(...this.userPlugins.map(p => p.name))
+    await this.userPlugins.update()
+    this.clearCache(...this.userPlugins.list.map(p => p.name))
   }
 
   async uninstall (name: string) {
-    let unlock = await lock.write(this.lockfile, {skipOwnPid: true})
-    let plugin = this.plugins.filter(p => !['core', 'builtin'].includes(p.type)).find(p => p.name === name)
+    let plugin = this.plugins.filter(p => ['user', 'link'].includes(p.type)).find(p => p.name === name)
     if (!plugin) throw new Error(`${name} is not installed`)
     switch (plugin.type) {
       case 'user': {
         if (!this.config.debug) this.out.action.start(`Uninstalling plugin ${name}`)
-        await this.yarn.exec(['remove', name])
+        await this.userPlugins.remove(name)
         break
       }
       case 'link': {
@@ -330,22 +279,11 @@ export default class Plugins {
       }
     }
     this.clearCache(name)
-    await unlock()
     this.out.action.stop()
   }
 
   addPackageToPJSON (name: string, version: string = '*') {
-    let pjson = this.userPluginsPJSON
-    if (!pjson.dependencies) pjson.dependencies = {}
-    pjson.dependencies[name] = version
-    this.saveUserPluginsPJSON(pjson)
-  }
-
-  removePackageFromPJSON (name: string) {
-    let pjson = this.userPluginsPJSON
-    if (!pjson.dependencies) pjson.dependencies = {}
-    delete pjson.dependencies[name]
-    this.saveUserPluginsPJSON(pjson)
+    this.userPlugins.addPackageToPJSON(name, version)
   }
 
   async addLinkedPlugin (p: string) {
@@ -360,17 +298,9 @@ export default class Plugins {
     this.cache.deletePlugin(...names)
   }
 
-  get userPluginsDir (): string { return path.join(this.config.dataDir, 'plugins') }
-  get userPluginsPJSONPath (): string { return path.join(this.userPluginsDir, 'package.json') }
-  userPluginPath (name: string): string { return path.join(this.userPluginsDir, 'node_modules', name) }
-
   get topics (): CachedTopic[] {
     return uniqby(this.plugins.reduce((t, p) => t.concat(p.topics), []), 'topic')
   }
 
-  get lockfile (): string { return path.join(this.config.cacheDir, 'plugins.lock') }
-
   config: Config
 }
-
-const entries = <T> (o: {[k: string]: T}): [string, T][] => Object.keys(o).map(k => [k, o[k]])
