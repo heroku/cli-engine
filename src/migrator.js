b486e606a8cd7b4a7f521c53a92031468c7d4049
RasPhilCo <capt.pn@gmail.com>
2017-04-19 20:51:59 -0700

    Migrate/try only once (#65)

--- src/migrator.js
+++ src/migrator.js
@@ -1,56 +1,68 @@
 // @flow
 
-import Command from 'cli-engine-command'
+import {type Config} from 'cli-engine-config'
+import Output from 'cli-engine-command/lib/output'
 import Plugins from './plugins'
+import UserPlugins from './user_plugins'
 import path from 'path'
 import fs from 'fs-extra'
 
-export default class extends Command {
+export default class {
   plugins: Plugins
+  userPlugins: UserPlugins
+  config: Config
+  out: Output
+
+  constructor (plugins: Plugins, config: Config) {
+    this.plugins = plugins
+    this.config = config
+    this.userPlugins = plugins.userPlugins
+    this.out = plugins.out
+  }
 
   async run () {
-    this.plugins = new Plugins(this.out)
-    let file = path.join(this.config.dataDir, 'plugins', 'plugins.json')
-    let pljson = await this.getPluginsJson(file)
+    if (fs.existsSync(this.userPlugins.userPluginsPJSONPath)) return
+    let pljson = await this._readPluginsJSON()
     if (!pljson) return
     this.out.debug('Migrating V5 plugins...')
     for (let p of pljson) {
       if (this.plugins.isPluginInstalled(p.name)) {
         this.out.debug(`Skipping already installed plugin: ${p.name}`)
       } else {
-        await this.installPlugin(p.name, p.tag)
+        await this._installPlugin(p.name, p.tag)
       }
     }
   }
 
-  async getPluginsJson (file: string) {
+  async _readPluginsJSON () {
     try {
-      return fs.readJSONSync(file)
+      let pljsonPath = path.join(this.userPlugins.userPluginsDir, 'plugins.json')
+      return fs.readJSONSync(pljsonPath)
     } catch (err) {
       this.out.debug(err.message)
     }
   }
 
-  async installPlugin (name: string, tag: string) {
+  async _installPlugin (name: string, tag: string) {
     try {
       if (tag === 'symlink') {
-        await this.reinstallViaSymlink(name)
+        await this._reinstallViaSymlink(name)
       } else {
         if (tag === '') tag = 'latest'
-        await this.addToPJSON(name, tag)
+        await this._addToPJSON(name, tag)
       }
     } catch (err) {
       this.out.warn(err)
     }
   }
 
-  async reinstallViaSymlink (name: string) {
+  async _reinstallViaSymlink (name: string) {
     this.out.debug(`Installing via symlink: ${name}`)
-    let pluginPath = fs.realpathSync(path.join(this.config.dataDir, 'plugins', 'node_modules', name))
+    let pluginPath = fs.realpathSync(this.userPlugins.userPluginPath(name))
     await this.plugins.addLinkedPlugin(pluginPath)
   }
 
-  async addToPJSON (name: string, tag: string) {
+  async _addToPJSON (name: string, tag: string) {
     this.out.debug(`Adding to plugins pjson: ${name}`)
     this.plugins.addPackageToPJSON(name, tag)
   }
