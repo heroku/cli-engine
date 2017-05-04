0e68f6e39d8ac20b0667d02b10069b69a5ab7a7a
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2017-05-04 10:42:59 -0700

    edits

--- src/not_found.test.js
+++ src/not_found.test.js
@@ -4,12 +4,13 @@ import Output from 'cli-engine-command/lib/output'
 import NotFound from './not_found'
 
 test('it exits with 127', async () => {
-  expect.assertions(1)
+  expect.assertions(2)
   let output = new Output({mock: true})
   let nf = new NotFound(output, ['heroku', 'foo'])
   try {
     await nf.run()
   } catch (err) {
     expect(err.code).toEqual(127)
+    expect(output.stderr.output).toMatch(/^ ▸ +foo is not a cli-engine command.\n/)
   }
 })
