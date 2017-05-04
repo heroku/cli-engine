10d2132b33f009838fc6565afe0f121a6eb1529b
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2017-05-04 10:46:28 -0700

    edits

--- src/not_found.js
+++ src/not_found.js
@@ -35,9 +35,10 @@ export default class NotFound {
 
   async run () {
     let closest = this.closest(this.argv[1])
+    let binHelp = `${this.config.bin} help`
 
     let perhaps = closest ? `Perhaps you meant ${this.out.color.yellow(closest)}\n` : ''
     this.out.error(`${this.out.color.yellow(this.argv[1])} is not a ${this.config.bin} command.
-${perhaps}Run ${this.out.color.cmd(`${this.config.bin} help`)} for a list of available commands.`, 127)
+${perhaps}Run ${this.out.color.cmd(binHelp)} for a list of available commands.`, 127)
   }
 }
