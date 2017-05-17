b2c035830ee5b75a666178810163dc9aa60aa63a
Ransom Briggs <rbriggs@heroku.com>
2017-05-17 14:06:52 -0500

    Fix usage for help for legacy plugins (#132)
    * Fix usage for help for legacy plugins
    
    * Remove globals comment from help.test.js

--- src/commands/help.test.js
+++ src/commands/help.test.js
@@ -2,6 +2,21 @@
 
 import Command from './help'
 
+import {buildConfig} from 'cli-engine-config'
+import Output from 'cli-engine-command/lib/output'
+
+const path = require('path')
+const fs = require('fs-extra')
+
+function mockOutput () {
+  let testDir = path.join(path.dirname(__filename), '..', '..', 'test')
+  let root = path.join(testDir, 'roots', 'test-foo')
+  let pjson = fs.readJSONSync(path.join(root, 'package.json'))
+  let config = buildConfig({root, pjson})
+
+  return new Output({config, mock: true})
+}
+
 test('shows the topics', async function () {
   let cmd = await Command.mock()
   expect(cmd.out.stdout.output).toMatch(/^ plugins +# manage plugins$/m)
@@ -10,4 +25,14 @@ test('shows the topics', async function () {
 test('shows help about plugins', async function () {
   let cmd = await Command.mock('plugins')
   expect(cmd.out.stdout.output).toMatch(/^ +plugins:install PLUGIN +# installs a plugin into the CLI$/m)
+})
+
+test('help should show usage in topics', async () => {
+  let cmd = await Command.run({argv: ['foo'], output: mockOutput()})
+  expect(cmd.out.stdout.output).toMatch(/^ fuzz:bar \[FUZZ|BAR\] # usage description$/m)
+})
+
+test('help should show usage in commands', async () => {
+  let cmd = await Command.run({argv: ['foo:usage'], output: mockOutput()})
+  expect(cmd.out.stdout.output).toMatch(/^ fuzz:bar \[FUZZ|BAR\]$/m)
 })
---
+++ test/roots/test-foo/node_modules/test-foo/commands/foo/usage.js
@@ -0,0 +1,7 @@
+module.exports = {
+  topic: 'foo',
+  command: 'usage',
+  usage: 'fuzz:bar [FUZZ|BAR]',
+  description: 'usage description',
+  run: function (context) {}
+}
