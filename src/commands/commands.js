56b517ca1d4d32fe9973792afb98732e0e3613dd
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2017-06-02 13:30:34 -0700

    added command command

---
+++ src/commands/commands.js
@@ -0,0 +1,25 @@
+// @flow
+
+import Command, {flags} from 'cli-engine-command'
+import Plugins from '../plugins'
+
+export default class Commands extends Command {
+  static topic = 'commands'
+  static hidden = true
+  static flags = {json: flags.boolean()}
+
+  async run () {
+    this.out.warn('heroku-cli: This CLI is deprecated. Please reinstall from https://cli.heroku.com')
+    let plugins = new Plugins(this.out)
+    let topics = plugins.topics.filter(t => !t.hidden)
+    let commands = plugins.commands.map(c => ({
+      command: c.command,
+      topic: c.topic,
+      usage: c.usage,
+      summary: c.description,
+      help: c.help,
+      hidden: c.hidden
+    }))
+    this.out.styledJSON({topics, commands})
+  }
+}
