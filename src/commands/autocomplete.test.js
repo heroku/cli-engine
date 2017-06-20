05950e4eb4d27511525776208201bcceea2e16cd
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2017-06-20 16:15:43 -0700

    add test

--- src/commands/autocomplete.test.js
+++ src/commands/autocomplete.test.js
@@ -1,8 +1,11 @@
 // @flow
 
 import Autocomplete from './autocomplete'
+import os from 'os'
 
 test('outputs commands file path', async () => {
+  // autocomplete will throw error on windows
+  if (os.platform() === 'windows') return
   let cmd = await Autocomplete.mock('--commands')
   expect(cmd.out.stdout.output).toMatch(/client\/node_modules\/cli-engine\/autocomplete\/commands\n/)
 })
