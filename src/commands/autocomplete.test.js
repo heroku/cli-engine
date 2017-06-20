4a2799d4363000754b2b7964c352411ad0d7224f
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2017-06-20 16:29:05 -0700

    add test

--- src/commands/autocomplete.test.js
+++ src/commands/autocomplete.test.js
@@ -4,7 +4,7 @@ import Autocomplete from './autocomplete'
 import os from 'os'
 
 // autocomplete will throw error on windows
-let skipWindows = os.platform() === 'windows' ? xtest : test
+let skipWindows = (os.platform() === 'windows' || os.platform() === 'win32') ? xtest : test
 
 skipWindows('outputs commands file path', async () => {
   let cmd = await Autocomplete.mock('--commands')
