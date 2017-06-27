b9d03bc697f191cc7bc071d84fd2a41cf01ab4b8
Ransom Briggs <rbriggs@heroku.com>
2017-06-27 16:25:40 -0500

    Wrap migrator in a lockfile (#218)

--- src/plugins/migrator.js
+++ src/plugins/migrator.js
@@ -6,6 +6,7 @@ import LinkedPlugins from './linked'
 import PluginCache from './cache'
 import path from 'path'
 import fs from 'fs-extra'
+import Lock from '../lock'
 
 const debug = require('debug')('cli-engine:migrator')
 
@@ -13,19 +14,33 @@ export default class {
   userPlugins: UserPlugins
   linkedPlugins: LinkedPlugins
   out: Output
+  lock: Lock
 
   constructor (out: Output) {
     let cache = new PluginCache(out)
     this.userPlugins = new UserPlugins({out, config: out.config, cache})
     this.linkedPlugins = new LinkedPlugins({out, config: out.config, cache})
     this.out = out
+    this.lock = new Lock(this.out)
   }
 
   async run () {
+    // short circuit quickly without having to aquire the writer lock
     if (fs.existsSync(this.userPlugins.userPluginsPJSONPath)) return
     if (!fs.existsSync(path.join(this.userPlugins.userPluginsDir, 'plugins.json'))) return
+
     let pljson = await this._readPluginsJSON()
     if (!pljson) return false
+
+    let downgrade = await this.lock.upgrade()
+    await this._run(pljson)
+    await downgrade()
+  }
+
+  async _run (pljson: any) {
+    // prevent two parallel migrations from happening in case of a race
+    if (fs.existsSync(this.userPlugins.userPluginsPJSONPath)) return
+
     debug('has existing plugins')
     this.out.action.start('Migrating Heroku CLI v5 plugins')
     debug('removing existing node_modules')
