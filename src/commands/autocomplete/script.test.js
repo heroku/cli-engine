cb57018d3534a8516db1f9b621f49bc50cdb7745
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2017-07-20 10:44:13 -0700

    remove only

--- src/commands/autocomplete/script.test.js
+++ src/commands/autocomplete/script.test.js
@@ -6,7 +6,7 @@ import os from 'os'
 // autocomplete will throw error on windows
 let skipWindows = (os.platform() === 'windows' || os.platform() === 'win32') ? xtest : test
 
-skipWindows.only('outputs autocomplete script for .zshrc', async () => {
+skipWindows('outputs autocomplete script for .zshrc', async () => {
   let cmd = await AutocompleteScript.mock('zsh')
   expect(cmd.out.stdout.output).toMatch(/fpath=\(/)
   expect(cmd.out.stdout.output).toMatch(/\/cli-engine\/autocomplete\/zsh/)
