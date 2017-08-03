94c34d52f8d6f8cb221c1d91d16733af018c5577
RasPhilCo <capt.pn@gmail.com>
2017-08-03 11:27:23 -0700

    Prefer linked plugins (#259)
    * stash
    
    * prefer linked plugins
    
    * fix lint

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -159,12 +159,6 @@ export default class Plugins {
     let downgrade = await this.lock.upgrade()
 
     await this.load()
-    let name = this.linked.checkLinked(p)
-
-    if (this.plugins.find(p => p.type === 'user' && p.name === name)) {
-      throw new Error(`${name} is already installed.\nUninstall with ${this.out.color.cmd(this.config.bin + ' plugins:uninstall ' + name)}`)
-    }
-
     Namespaces.throwErrorIfNotPermitted(p, this.config)
 
     await this.linked.add(p)
