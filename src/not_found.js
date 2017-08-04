c0fc76aa9214b13bd2dab6d89bb420ad1f826f38
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2017-08-04 13:47:09 -0700

    improve not_found

--- src/not_found.js
+++ src/not_found.js
@@ -24,20 +24,26 @@ export default class NotFound {
   }
 
   closest (cmd: string) {
-    const LST = require('levenshtein')
-    let max
-    for (let c of this.allCommands()) {
-      if (!c) continue
-      let d = new LST(cmd, c)
-      if (!max || d.distance < max[1]) max = [c, d.distance]
-    }
-    return max ? max[0] : null
+    const DCE = require('string-similarity')
+    return DCE.findBestMatch(cmd, this.allCommands()).bestMatch.target
+  }
+
+  async isTopic (name: string) : Promise<boolean> {
+    let t = await this.plugins.findTopic(name)
+    return !!t
   }
 
   async run () {
     await this.plugins.load()
-    let closest = this.closest(this.argv[1])
-    let binHelp = `${this.config.bin} help`
+    let id = this.argv[1]
+    let closest
+    let binHelp
+    if (await this.isTopic(id)) {
+      binHelp = `${this.config.bin} help ${id}`
+    } else {
+      closest = this.closest(id)
+      binHelp = `${this.config.bin} help`
+    }
 
     let perhaps = closest ? `Perhaps you meant ${this.out.color.yellow(closest)}\n` : ''
     this.out.error(`${this.out.color.yellow(this.argv[1])} is not a ${this.config.bin} command.
