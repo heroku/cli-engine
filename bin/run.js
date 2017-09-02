1c5e1f64552a20d93864d8a768eed730498c61f4
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-09-01 19:27:57 -0700

    simplify run

--- bin/run.js
+++ bin/run.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const root = path.join(__dirname, '..', 'example')
-const {run} = require('../src/cli.js')
+const {run} = require('../src/cli')
 run({
   config: {
     root,
