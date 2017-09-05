93152828189d1d31468443cf15835680b3026fd9
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-09-04 20:10:26 -0700

    dispatch work

--- src/dispatcher.js
+++ src/dispatcher.js
@@ -1,61 +1,75 @@
 // @flow
 
 import {type Config} from 'cli-engine-config'
-import {default as CommandBase} from 'cli-engine-command'
+import {type Command} from 'cli-engine-command'
 import path from 'path'
 import {undefault} from './util'
 const debug = require('debug')('cli:dispatcher')
 
-const builtins = {
-  version: 'version'
+class CommandManagerBase {
+  config: Config
+  constructor (config: Config) {
+    this.config = config
+  }
+  findCommand (cmd: string): ?Class<Command<*>> {
+    return null
+  }
+}
+
+class BuiltinCommandManager extends CommandManagerBase {
+  findCommand (cmd) {
+    const builtins = {
+      version: 'version',
+      help: 'help'
+    }
+
+    let p = builtins[cmd]
+    if (p) {
+      p = path.join(__dirname, 'commands', p)
+      return require(p)
+    }
+  }
+}
+
+class CLICommandManager extends CommandManagerBase {
+  findCommand (cmd) {
+    let root = this.config.commandsDir
+    let p
+    try {
+      debug(`finding ${cmd} command`)
+      p = require.resolve(path.join(root, ...cmd.split(':')))
+    } catch (err) {
+      if (err.code !== 'MODULE_NOT_FOUND') throw err
+    }
+    if (p) return require(p)
+  }
 }
 
 export default class Dispatcher {
   config: Config
   cmd: string
+  managers: CommandManagerBase[]
   constructor (config: Config) {
     this.config = config
+    this.managers = [
+      new BuiltinCommandManager(config),
+      new CLICommandManager(config)
+    ]
   }
 
   run (...argv: string[]) {
     let argv0 = argv.shift()
     debug('argv0: %s', argv0)
     this.cmd = argv.shift()
-    let Command: ?Class<CommandBase<*>>
-    Command = this.findCommand()
-    if (!Command) throw new Error(`${this.cmd} command not found`)
-    Command.run({config: this.config, argv})
+    let command = this.findCommand()
+    if (!command) throw new Error(`${this.cmd} command not found`)
+    return command.run({config: this.config, argv})
   }
 
   findCommand () {
-    let p = this.findCommandInCLI() || this.findCommandInBuiltins()
-    if (p) {
-      debug('loading command from %s', p)
-      return undefault(require(p))
-    }
-  }
-
-  findCommandInCLI () {
-    let root = this.config.commandsDir
-    try {
-      if (!this.cmd) {
-        debug('loading root command from %s', this.cmd)
-        // TODO: make flag parsing work here somehow
-        return require.resolve(root)
-      } else {
-        debug(`finding ${this.cmd} command`)
-        return require.resolve(path.join(root, ...this.cmd.split(':')))
-      }
-    } catch (err) {
-      if (err.code !== 'MODULE_NOT_FOUND') throw err
-    }
-  }
-
-  findCommandInBuiltins () {
-    let p = builtins[this.cmd]
-    if (p) {
-      p = path.join(__dirname, 'commands', p)
-      return require.resolve(p)
+    for (let manager of this.managers) {
+      let command = manager.findCommand(this.cmd)
+      if (command) return undefault(command)
     }
   }
 }
