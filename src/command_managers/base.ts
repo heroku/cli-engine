9d07951ffcb2a9624b37285e973980403a9e676e
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-09-13 19:26:25 -0700

    wip

--- src/command_managers/base.ts
+++ src/command_managers/base.ts
@@ -7,44 +7,71 @@ import _ from '../lodash'
 
 const debug = require('debug')('cli:commands')
 
+function uniqTopics (topics: Topic[]): Topic[] {
+  topics = _.sortBy(topics, t => [t.name, t.hidden, t.description])
+  topics = _.sortedUniqBy(topics, t => t.name)
+  return topics
+}
+
+function uniqCommandIDs (ids: string[]): string[] {
+  ids = ids.sort()
+  ids = _.sortedUniqBy(ids, t => t)
+  return ids
+}
+
 export abstract class CommandManagerBase {
   config: Config
   cli: CLI
+  submanagers: CommandManagerBase[] = []
 
-  constructor({ config, cli }: { config: Config; cli: CLI }) {
+  constructor({ config, cli }: { config: Config; cli?: CLI }) {
+    this.cli = cli || new CLI({debug: !!config.debug, mock: config.mock, errlog: config.errlog})
     this.config = config
-    this.cli = cli
   }
 
-  abstract findCommand(id: string): Promise<ICommand | undefined>
-  abstract listTopics(): Promise<Topic[]>
-  abstract listCommandIDs(): Promise<string[]>
-
-  require(p: string, id: string): ICommand {
-    debug('Reading command %s at %s', id, p)
-    let Command: undefined | typeof CommandBase
-    try {
-      Command = deps.util.undefault(require(p))
-    } catch (err) {
-      this.cli.warn(err, {context: `Error reading command from ${p}`})
+  public async findCommand(id: string): Promise<ICommand | undefined> {
+    await this.init()
+    let finders = this.submanagers.map(m => m.findCommand(id))
+    for (let p of finders) {
+      let Command = await p
+      if (Command) return Command
     }
-    if (!Command || !(Command.prototype instanceof CommandBase)) {
-      let extra = deps.util.isEmpty(Command)
-        ? 'Does the command have `module.exports = MyCommand`?'
-        : `Received: ${inspect(Command)}`
-      throw new Error(`${p} does not appear to be a valid command.\n${extra}`)
-    }
-    const command = new Command(this.config)
-    command.__config.id = id
-    return command
   }
 
-  async findTopic(id: string): Promise<Topic | undefined> {
+  public async listTopics(): Promise<Topic[]> {
+    await this.init()
+    let arrs = await Promise.all(this.submanagers.map(m => m.listTopics()))
+    let topics = arrs.reduce((next, res) => next.concat(res), [])
+    return uniqTopics(topics)
+  }
+
+  public async listCommandIDs(): Promise<string[]> {
+    await this.init()
+    let arrs = await Promise.all(this.submanagers.map(m => m.listCommandIDs()))
+    let ids = arrs.reduce((next, res) => next.concat(res), [])
+    return uniqCommandIDs(ids)
+  }
+
+  public async findTopic(id: string): Promise<Topic | undefined> {
+    await this.init()
     let topics = await this.listTopics()
     return topics.find(t => t.name === id)
   }
 
-  async listTopicIDs(): Promise<string[]> {
+  public async listRootCommands(): Promise<ICommand[]> {
+    await this.init()
+    const ids = await this.listRootCommandIDs()
+    let commands = _.compact(await Promise.all(ids.map(id => this.findCommand(id))))
+    commands = _.sortBy(commands, c => c.__config.id)
+    commands = _.sortedUniqBy(commands, c => c.__config.id)
+    return commands
+  }
+
+  protected async init (): Promise<void> {
+    await Promise.all(this.submanagers.map(m => m.init()))
+  }
+
+  protected async listTopicIDs(): Promise<string[]> {
     const commands = await this.listCommandIDs()
     const topics = _.uniq(
       commands
@@ -65,19 +92,15 @@ export abstract class CommandManagerBase {
     return topics
   }
 
-  async commandsForTopic(topic: string): Promise<ICommand[]> {
+  protected async commandsForTopic(topic: string): Promise<ICommand[]> {
     let ids = await this.commandIDsForTopic(topic)
-    let commands = await Promise.all(ids.map(id => this.findCommand(id)))
-    return _.compact(commands)
+    let commands = _.compact(await Promise.all(ids.map(id => this.findCommand(id))))
+    commands = _.sortBy(commands, c => c.__config.id)
+    commands = _.sortedUniqBy(commands, c => c.__config.id)
+    return commands
   }
 
-  async listRootCommands(): Promise<ICommand[]> {
-    const ids = await this.listRootCommandIDs()
-    let commands = await Promise.all(ids.map(id => this.findCommand(id)))
-    return _.compact(commands)
-  }
-
-  async commandIDsForTopic(topic: string): Promise<string[]> {
+  protected async commandIDsForTopic(topic: string): Promise<string[]> {
     let ids = await this.listCommandIDs()
     // show matching commands
     ids = ids.filter(id => id === topic || id.startsWith(`${topic}:`))
@@ -86,8 +109,27 @@ export abstract class CommandManagerBase {
     return ids
   }
 
-  async listRootCommandIDs(): Promise<string[]> {
+  protected async listRootCommandIDs(): Promise<string[]> {
     let ids = await this.listCommandIDs()
     return ids.filter(id => !id.includes(':'))
+  }
+
+  protected require(p: string, id: string): ICommand {
+    debug('Reading command %s at %s', id, p)
+    let Command: undefined | typeof CommandBase
+    try {
+      Command = deps.util.undefault(require(p))
+    } catch (err) {
+      this.cli.warn(err, {context: `Error reading command from ${p}`})
+    }
+    if (!Command || !(Command.prototype instanceof CommandBase)) {
+      let extra = deps.util.isEmpty(Command)
+        ? 'Does the command have `module.exports = MyCommand`?'
+        : `Received: ${inspect(Command)}`
+      throw new Error(`${p} does not appear to be a valid command.\n${extra}`)
+    }
+    const command = new Command(this.config)
+    command.__config.id = id
+    return command
   }
 }
--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,29 +1,25 @@
-import {Config} from 'cli-engine-config'
 import {CLI} from 'cli-ux'
+import {CommandManagerBase} from '../command_managers/base'
+import {Config} from 'cli-engine-config'
 import * as fs from 'fs-extra'
 import * as path from 'path'
+import _ from 'ts-lodash'
 
-export class Plugins {
-  private cli: CLI
-  private plugins: Plugin[]
-
-  constructor (readonly config: Config, cli?: CLI) {
-    this.cli = cli || new CLI({mock: config.mock, debug: !!config.debug, errlog: config.errlog})
-  }
-
-  public async listCommandIDs (): Promise<string[]> {
-    await this.load()
-    return ['foo']
-  }
-
-  private async load () {
-    if (this.plugins) return
-    this.plugins = []
+export class Plugins extends CommandManagerBase {
+  protected async init () {
+    if (this.submanagers.length) return
     const cli = this.config.pjson['cli-engine']
     if (cli.plugins) {
-      this.plugins.concat(cli.plugins.map(p => new Plugin({type: 'core', root: path.join(this.config.root, 'node_modules', p)})))
+      let pluginOpts = {cli: this.cli, config: this.config}
+      let plugins = cli.plugins.map(p => {
+        return new Plugin({
+          ...pluginOpts,
+          type: 'core',
+          root: path.join(this.config.root, 'node_modules', p)
+        })
+      })
+      this.submanagers = this.submanagers.concat(plugins)
     }
-    await Promise.all(this.plugins.map(p => p.load()))
   }
 }
 
@@ -31,24 +27,57 @@ type PluginTypes = 'core' | 'user' | 'link'
 type PluginOptions = {
   type: PluginTypes
   root: string
+  config: Config
+  cli: CLI
 }
 
 type PluginPJSON = {
   name: string
   version: string
+  main?: string
 }
 
-class Plugin {
+type PluginCommand = {
+  topic?: string
+  command?: string
+}
+
+type PluginModule = {
+  commands: PluginCommand[]
+}
+
+class Plugin extends CommandManagerBase {
   public type: PluginTypes
   public root: string
   public pjson: PluginPJSON
+  public module?: PluginModule
 
   constructor (options: PluginOptions) {
+    super(options)
     this.type = options.type
     this.root = options.root
   }
 
-  public async load () {
+  public async listCommandIDs (): Promise<string[]> {
+    let ids = await super.listCommandIDs()
+    if (this.module) {
+      ids = ids.concat(this.module.commands.map(c => this.makeID(c)))
+    }
+    return ids
+  }
+
+  protected async init () {
     this.pjson = await fs.readJSON(path.join(this.root, 'package.json'))
+    if (this.pjson.main) {
+      const m = require(path.join(this.root, this.pjson.main))
+      if (!m.commands) m.commands = []
+      if (!m.topics) m.topics = []
+      if (m.topic) m.topics.push(m.topic)
+      this.module = m
+    }
+  }
+
+  private makeID (c: PluginCommand): string {
+    return _.compact([c.topic, c.command]).join(':')
   }
 }
