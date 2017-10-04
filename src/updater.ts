4660206d93baafb9a32526c33342c0b5f784ace3
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-10-03 21:28:24 -0700

    show error

--- src/updater.ts
+++ src/updater.ts
@@ -280,6 +280,7 @@ export class Updater {
       return mtime(this.autoupdatefile).isBefore(deps.moment().subtract(5, 'hours'))
     } catch (err) {
       if (err.code !== 'ENOENT') console.error(err.stack)
+      debug(err)
       return true
     }
   }
