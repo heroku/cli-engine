1ab91fd342d2e3c53735069fa9007ca3f662e7d1
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-10-03 20:24:56 -0700

    added --v alias

--- src/commands/version.ts
+++ src/commands/version.ts
@@ -3,7 +3,7 @@ import { Command } from 'cli-engine-command'
 export default class Version extends Command {
   options = {
     description: 'show CLI version',
-    aliases: ['-v', 'v', '--version'],
+    aliases: ['-v', 'v', '--version', '--v'],
   }
 
   async run() {
