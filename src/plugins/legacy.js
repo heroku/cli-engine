ca5c133abfd9ebbe3f6f5b5cf1d1d09d1845c036
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2017-12-11 16:29:18 -0800

    convert from cli legacy converter

--- src/plugins/legacy.js
+++ src/plugins/legacy.js
@@ -1,8 +1,5 @@
 // @flow
 
-import {type Flag} from 'cli-engine-config'
-import {flags as Flags} from 'cli-engine-command'
-
 export type LegacyFlag = {
   name: string,
   description?: string,
@@ -12,22 +9,4 @@ export type LegacyFlag = {
   required?: boolean,
   optional?: boolean,
   parse?: any
-}
-
-export function convertFlagsFromV5 (flags: ?(LegacyFlag[] | {[name: string]: Flag})): {[name: string]: any} {
-  if (!flags) return {}
-  if (!Array.isArray(flags)) return flags
-  return flags.reduce((flags, flag) => {
-    let opts: Flag = {
-      char: (flag.char: any),
-      description: flag.description,
-      hidden: flag.hidden,
-      required: flag.required,
-      optional: flag.optional,
-      parse: flag.parse
-    }
-    Object.keys(opts).forEach(k => opts[k] === undefined && delete opts[k])
-    flags[flag.name] = flag.hasValue ? Flags.string(opts) : Flags.boolean((opts: any))
-    return flags
-  }, {})
 }
