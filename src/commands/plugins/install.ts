82d79ebb23faceea6a590afd432975c57f6e901a
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-12-24 16:04:47 -0800

    install multiple plugins at once

--- src/commands/plugins/install.ts
+++ src/commands/plugins/install.ts
@@ -18,10 +18,12 @@ export default class PluginsInstall extends Command {
   static topic = 'plugins'
   static command = 'install'
   static description = 'installs a plugin into the CLI'
+  static usage = 'plugins:install PLUGIN...'
   static help = `
   Example:
     $ ${bin} plugins:install ${examplePlugin}
   `
+  static variableArgs = true
   static args = [{ name: 'plugin', description: 'plugin to install', required: true }]
   static flags: flags.Input = {
     force: flags.boolean({ char: 'f' }),
@@ -29,9 +31,11 @@ export default class PluginsInstall extends Command {
 
   async run() {
     const plugins = new Plugins(this.config)
-    const [plugin, tag = 'latest'] = this.argv[0].split('@')
-    if (!this.config.debug) cli.action.start(`Installing plugin ${plugin}${tag === 'latest' ? '' : '@' + tag}`)
-    await plugins.install({ type: 'user', name: plugin, tag, force: this.flags.force })
+    for (let plugin of this.argv) {
+      const [name, tag = 'latest'] = plugin.split('@')
+      if (!this.config.debug) cli.action.start(`Installing plugin ${name}${tag === 'latest' ? '' : '@' + tag}`)
+      await plugins.install({ type: 'user', name, tag, force: this.flags.force })
+    }
     const hooks = new Hooks(this.config)
     await hooks.run('update')
   }
