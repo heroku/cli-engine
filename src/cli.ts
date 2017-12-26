c3152664b97fc55fc4193dfe31c82e15bb39bebd
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-12-26 01:34:50 -0800

    move setup stuff into class

--- src/cli.ts
+++ src/cli.ts
@@ -5,33 +5,12 @@ import { color } from 'heroku-cli-color'
 import * as path from 'path'
 import deps from './deps'
 
-const debug = require('debug')('cli')
-const handleEPIPE = (err: Error) => {
-  if ((err as any).code !== 'EPIPE') throw err
-}
-
-const g = global as any
-if (!g.testing) {
-  process.once('SIGINT', () => {
-    if (cli.action.task) cli.action.stop(color.red('ctrl-c'))
-    cli.exit(1)
-  })
-  let handleErr = (err: Error) => {
-    cli.error(err)
-  }
-  process.once('uncaughtException', handleErr)
-  process.once('unhandledRejection', handleErr)
-  process.stdout.on('error', handleEPIPE)
-  process.stderr.on('error', handleEPIPE)
-}
-
-process.env.CLI_ENGINE_VERSION = require('../package.json').version
-
 export default class CLI {
   constructor(private config: IConfig) {}
 
-  async run(argv: string[]): Promise<any> {
-    debug('starting run: %o', argv)
+  public async run(argv: string[]): Promise<any> {
+    this.debug('starting run: %o', argv)
+    if (!this.global.testing) this.setupHandlers()
     const id = argv[2]
 
     if (id !== 'update') {
@@ -45,8 +24,35 @@ export default class CLI {
     return result
   }
 
+  private get debug () {
+    return require('debug')('cli')
+  }
+
+  private get global(): {testing?: boolean} {
+    return global as any
+  }
+
+  private setupHandlers () {
+    process.env.CLI_ENGINE_VERSION = require('../package.json').version
+    if (this.global.testing) return
+    process.once('SIGINT', () => {
+      if (cli.action.task) cli.action.stop(color.red('ctrl-c'))
+      cli.exit(1)
+    })
+    let handleErr = (err: Error) => {
+      cli.error(err)
+    }
+    process.once('uncaughtException', handleErr)
+    process.once('unhandledRejection', handleErr)
+    const handleEPIPE = (err: Error) => {
+      if ((err as any).code !== 'EPIPE') throw err
+    }
+    process.stdout.on('error', handleEPIPE)
+    process.stderr.on('error', handleEPIPE)
+  }
+
   private async exitAfterStdoutFlush() {
-    if (g.testing) return
+    if (this.global.testing) return
     const { timeout } = require('./util')
     await timeout(this.flush(), 10000)
   }
