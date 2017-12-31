2dc47d94179cf407fe75bcd294918957cd2c783e
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-12-30 20:19:45 -0800

    remove async for node 6 compat

--- src/__test__/setup.js
+++ src/__test__/setup.js
@@ -1,9 +1,9 @@
-module.exports = async () => {
+module.exports = () => {
   const path = require('path')
   const fs = require('fs-extra')
 
   const testRoot = path.join(__dirname, '../../tmp/test')
-  await fs.remove(testRoot)
   process.env.CLI_ENGINE_DATA_DIR = path.join(testRoot, 'data')
   process.env.CLI_ENGINE_CACHE_DIR = path.join(testRoot, 'cache')
+  return fs.remove(testRoot)
 }
