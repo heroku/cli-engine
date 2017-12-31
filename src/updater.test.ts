4487437e741e7c8455010fe447765401ad5ed451
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-12-31 01:15:05 -0800

    fix test on linux

--- src/updater.test.ts
+++ src/updater.test.ts
@@ -9,22 +9,25 @@ import * as fs from './file'
 let api = nock('https://cli-assets.heroku.com')
 
 beforeEach(() => nock.cleanAll())
-afterEach(() => api.done())
+afterEach(() => {
+  delete process.env.CLI_ENGINE_PLATFORM
+  delete process.env.CLI_ENGINE_ARCH
+  api.done()
+})
 
 skipIfWin32('updates the CLI on unix', async () => {
+  process.env.CLI_ENGINE_PLATFORM = 'darwin'
+  process.env.CLI_ENGINE_ARCH = 'x64'
   const config = new Config()
   api
-    .get(`/cli-engine-example/channels/stable/${config.platform}-${config.arch}`)
+    .get(`/cli-engine-example/channels/stable/darwin-x64`)
     .reply(200, {
       channel: 'stable',
       version: '1.2.3',
       sha256gz: '83ee9ce76e691208f39716d74a8944bfcc6ba5358ec23d43034d13727524aee2',
     })
-    .get(`/cli-engine-example/channels/stable/cli-engine-example-v1.2.3-${config.platform}-${config.arch}.tar.gz`)
-    .replyWithFile(
-      200,
-      path.join(__dirname, `./__test__/fixtures/cli-engine-example-v1.2.3-${config.platform}-${config.arch}.tar.gz`),
-    )
+    .get(`/cli-engine-example/channels/stable/cli-engine-example-v1.2.3-darwin-x64.tar.gz`)
+    .replyWithFile(200, path.join(__dirname, `./__test__/fixtures/cli-engine-example-v1.2.3-darwin-x64.tar.gz`))
     .get(`/cli-engine-example/channels/stable/version`)
     .reply(200, { channel: 'stable', version: '1.2.3' })
 
@@ -55,9 +58,6 @@ test('updates the CLI on windows', async () => {
   expect(await fs.readFile(path.join(config.dataDir, 'client/bin/cli-engine.cmd'))).toEqual(
     `@echo off\n\"%~dp0\\..\\cli-engine-example-v1.2.3-win32-x64\\bin\\cli-engine.cmd\" %*\n`,
   )
-
-  delete process.env.CLI_ENGINE_PLATFORM
-  delete process.env.CLI_ENGINE_ARCH
 })
 
 test('validates the sha', async () => {
